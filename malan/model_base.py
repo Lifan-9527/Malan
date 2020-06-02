650b944b9c92246a266e1a4f556954f7b9aadf09
Lifan-9527 <oppenheimli@tencent.com>
2020-06-02 08:04:44 +0800

    Add model base for quick test

--- malan/model_base.py
+++ malan/model_base.py
@@ -1,15 +1,16 @@
 import tensorflow as tf
 import rest
 
+
 class ModelBase(object):
     def __init__(self):
         """
         """
         pass
 
-    def build_graph(self):
+    def build_graph(self, io):
         """
         Pending to be overrided.
-        :return:
+        :return: labels, predict, loss
         """
         pass
